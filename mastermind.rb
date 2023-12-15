daabf99201a61b28c0c295c6ab7d0b4579af7326
averybrubeck <averybrubeck@yahoo.com>
2023-12-14 20:28:12 -0500

    added compare choices

--- mastermind.rb
+++ mastermind.rb
@@ -1,18 +1,26 @@
-# Game Loop\
+# Game Loop
 class Mastermind
   def generate_secret
     word_bank = %w[Red Blue Green Yellow Purple Orange Pink Brown Turquoise Lavender Indigo Cyan Maroon Olive Coral Gold
                    Silver Gray Teal Magenta]
-    secret = word_bank.sample.to_s.downcase
-    puts secret
+    @secret = word_bank.sample.to_s.downcase
+    puts @secret
   end
 
   def player_guess
-    guess = gets.chomp.to_s.downcase
-    puts guess
+    @guess = gets.chomp.to_s.downcase
+  end
+
+  def compare_choices
+    if @secret == @guess
+      puts 'The Guess is Correct!'
+    else
+      puts 'You Got It Wrong'
+    end
   end
 end
 
 m = Mastermind.new
 m.generate_secret
 m.player_guess
+m.compare_choices
