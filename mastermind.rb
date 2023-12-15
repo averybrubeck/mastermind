5c4464568d20dfb98e5c6bc19d90ff9baa4af344
averybrubeck <averybrubeck@yahoo.com>
2023-12-14 21:15:34 -0500

    added correct letter count

--- mastermind.rb
+++ mastermind.rb
@@ -24,13 +24,24 @@ class Mastermind
       end
 
       @turns -= 1
-      puts "You Got It Wrong, turns left #{@turns}"
+      correct_count = correct_letter_count
+      puts "You Got It Wrong, turns left #{@turns}, #{correct_count} of letters correct"
+      
 
       if @turns.zero?
         puts 'game over'
         break
       end
     end
+  end
+
+  def correct_letter_count
+    correct_count = 0
+
+    @secret.chars.each_with_index do |char, index|
+      correct_count += 1 if char == @guess[index]
+    end
+    correct_count
   end
 end
 
