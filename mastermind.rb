217ea7b41b401965c85b2dd687352e084f09437b
averybrubeck <averybrubeck@yahoo.com>
2023-12-14 20:19:43 -0500

    added generate secret and get player guess

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,18 @@
 # Game Loop\
-class Mastermind 
-    def initialize(secret, turns_left)
-        @secret = secret
-        @turns = turns_left
-    end
+class Mastermind
+  def generate_secret
+    word_bank = %w[Red Blue Green Yellow Purple Orange Pink Brown Turquoise Lavender Indigo Cyan Maroon Olive Coral Gold
+                   Silver Gray Teal Magenta]
+    secret = word_bank.sample.to_s.downcase
+    puts secret
+  end
+
+  def player_guess
+    guess = gets.chomp.to_s.downcase
+    puts guess
+  end
 end
+
+m = Mastermind.new
+m.generate_secret
+m.player_guess
