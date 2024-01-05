9a160c98e3e86d5151014e44a5c222b0b5406b4d
averybrubeck <averybrubeck@yahoo.com>
2024-01-04 21:48:56 -0500

    added more logic, refectord some code

--- mastermind.rb
+++ mastermind.rb
@@ -31,11 +31,67 @@ class Mastermind
 
   def generate_computer_guess
     current_word = @word_bank[@current_word_index]
-    letter_to_guess = current_word[@turns]
+    if current_word.nil?
+      puts 'Error: No more words in the word bank.'
+      return
+    end
+    guess = initial_guess(current_word.length)
+    loop do
+      feedback = get_feedback(guess)
+      break if feedback == current_word
+      prune_word_bank(guess, feedback)
+      guess = refine_guess(guess, feedback)
+    end
+    @current_word_index += 1
+    guess
+  end
+  def initial_guess(length)
+    ('a'..'z').to_a.sample(length).join('')
+  end
 
-    if letter_to_guess.nil?
-      @current_word_index += 1
-      return generate_computer_guess if @current_word_index < @word_bank.length
+  def get_feedback(guess)
+    puts "Computer's guess: #{guess}"
+    puts 'Enter feedback (e.g., "correct positions, correct letters"):'
+    gets.chomp.downcase
+  end
+
+  def prune_word_bank(guess, feedback)
+    @word_bank.reject! { |word| feedback(word, guess) != feedback }
+  end
+
+  def refine_guess(previous_guess, feedback)
+    # Here, you can implement a more sophisticated algorithm for refining the guess
+    # For simplicity, let's just generate a random guess with the same length
+    ('a'..'z').to_a.sample(previous_guess.length).join('')
+  end
+
+  def feedback(word, guess)
+    word.chars.zip(guess.chars).count { |w, g| w == g }.to_s + ', ' +
+      word.chars.uniq.count { |c| guess.include?(c) && word.count(c) == guess.count(c) }.to_s
+  end
+
+  def computer_game
+    loop do
+      player_generate_secret
+      @current_word_index = 0  # Reset index for a new game
+      @turns = 12  # Reset turns for a new game  
+      loop do
+        @guess = generate_computer_guess
+        if @guess == @secret
+          puts 'The Computer Has Won'
+          puts "#{@guess}"
+          break
+        end
+        @turns -= 1
+        if @turns.zero?
+          puts 'The Computer Has Run Out Of Turns'
+          puts "Secret was: #{@secret}"
+          break
+        end
+      end
+      puts 'Do you want to play again? (yes/no)'
+      play_again = gets.chomp.downcase
+      break if play_again != 'yes'
     end
   end
 
@@ -53,27 +109,14 @@ class Mastermind
       puts "You Got It Wrong, turns left #{@turns}, #{correct_count} of letters correct"
 
       if @turns.zero?
-        puts "game over the secret was #{@secret}"
+        puts "Game over. The secret was #{@secret}"
         break
       end
-    end
-  end
-
-  def computer_game
-    player_generate_secret
-    loop do
-      if @guess == generate_computer_guess
-        puts 'The Computer Has Won'
-        break
-      end
-      if @turns.zero?
-        puts 'The Computer Has Run Out Of Turns'
-        break
-      end
-      @turns -= 1
     end
   end
 end
 
+# Example usage
 game = Mastermind.new
 game.computer_game
+
